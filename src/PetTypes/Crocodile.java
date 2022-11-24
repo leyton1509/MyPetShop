4c6d4e5e264a545739382e27819961c28f48cf40
leyton1509 <taylor92@lancaster.ac.uk>
2022-11-24 14:48:07 +0000

    Animals with extend the pet class

---
+++ src/PetTypes/Crocodile.java
@@ -0,0 +1,34 @@
+package PetTypes;
+
+/**
+ * Crocodile class extending the pet class
+ */
+
+public class Crocodile extends Pet{
+
+    /**
+     * A representation of a dog
+     * Is a pet
+     * @param _animalName the name of animal
+     * @param _animalClass the class of the animal
+     * @param _animalAge the age of the animal
+     * @param _ownerName the name of the owner
+     * @param _animalWeight the weight of the animal
+     * @param _animalColour the colour of the animal
+     */
+
+    public Crocodile(String _animalName, String _animalClass, int _animalAge, String _ownerName, float _animalWeight, String _animalColour){
+        super  ( _animalName,  _animalClass, _animalAge, _ownerName, _animalWeight, _animalColour);
+    }
+
+    /**
+     * Overrides the makeAnimalNoise method in Pet
+     * Prints out hiss
+     */
+
+    public void makeAnimalNoise(){
+        System.out.println("HISS");
+    }
+
+
+}
---
+++ src/PetTypes/Dog.java
@@ -0,0 +1,30 @@
+package PetTypes;
+
+/**
+ * Dog class extending the pet class
+ */
+public class Dog extends Pet {
+
+    /**
+     * A representation of a dog
+     * Is a pet
+     * @param _animalName the name of animal
+     * @param _animalClass the class of the animal
+     * @param _animalAge the age of the animal
+     * @param _ownerName the name of the owner
+     * @param _animalWeight the weight of the animal
+     * @param _animalColour the colour of the animal
+     */
+    public Dog(String _animalName, String _animalClass, int _animalAge, String _ownerName, float _animalWeight, String _animalColour){
+        super  ( _animalName,  _animalClass, _animalAge, _ownerName, _animalWeight, _animalColour);
+    }
+
+    /**
+     * Overrides the makeAnimalNoise method in Pet
+     * Prints out bark
+     */
+    public void makeAnimalNoise(){
+        System.out.println("BARK");
+    }
+
+}
