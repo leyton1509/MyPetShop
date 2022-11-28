da697815ad411bfb5a663b6238df50a6cfaa3df8
leyton1509 <taylor92@lancaster.ac.uk>
2022-11-28 14:18:12 +0000

    Added constructor and extended dog

---
+++ src/PetTypes/DogTypes/GermanShepard.java
@@ -0,0 +1,10 @@
+package PetTypes.DogTypes;
+
+import PetTypes.Dog;
+
+public class GermanShepard extends Dog {
+
+    public GermanShepard(String _animalName, String _animalClass, int _animalAge, String _ownerName, float _animalWeight, String _animalColour, String _breed){
+        super  ( _animalName,  _animalClass, _animalAge, _ownerName, _animalWeight, _animalColour, _breed);
+    }
+}
