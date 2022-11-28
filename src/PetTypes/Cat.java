bb8d8ec7b9a9ff834f966dd2df56f7bc61c37bd8
leyton1509 <taylor92@lancaster.ac.uk>
2022-11-28 14:04:48 +0000

    Added a Cat class

---
+++ src/PetTypes/Cat.java
@@ -0,0 +1,32 @@
+package PetTypes;
+
+/**
+ * Dog class extending the pet class
+ */
+public class Cat extends Pet {
+
+    /**
+     * A representation of a cat
+     * Is a pet
+     * @param _animalName the name of animal
+     * @param _animalClass the class of the animal
+     * @param _animalAge the age of the animal
+     * @param _ownerName the name of the owner
+     * @param _animalWeight the weight of the animal
+     * @param _animalColour the colour of the animal
+     */
+    public Cat(String _animalName, String _animalClass, int _animalAge, String _ownerName, float _animalWeight, String _animalColour){
+        super  ( _animalName,  _animalClass, _animalAge, _ownerName, _animalWeight, _animalColour);
+    }
+
+    /**
+     * Overrides the makeAnimalNoise method in Pet
+     * Prints out bark
+     */
+
+    @Override
+    public void makeAnimalNoise(){
+        System.out.println("MEOW");
+    }
+
+}
