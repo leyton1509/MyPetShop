fb9f8436835824dccce16ca3ceae9f8a91cd48f0
leyton1509 <taylor92@lancaster.ac.uk>
2022-11-28 14:05:45 +0000

    Added getters for variables changed from protected to public

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,7 @@
 import PetTypes.Crocodile;
 import PetTypes.Dog;
 import PetTypes.Pet;
-import PetTypes.PetRecord;
-
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Driver {
 
@@ -13,7 +11,7 @@ public class Driver {
         // Main method for pet shop
 
         // Array list of pets
-        ArrayList<Pet> pets = new ArrayList<>();
+        LinkedList<Pet> pets = new LinkedList<>();
 
         // Crocodile Object
         Crocodile timmy = new Crocodile("Timmy", "Reptile", 27, "Sam", 280f, "Black");
@@ -23,6 +21,7 @@ public class Driver {
         pets.add(timmy);
 
         // Crocodile Object
+
         Dog sam = new Dog("Sam", "Mammal", 8, "Dominick", 27.90f, "White");
 
         // See if the age can be set
@@ -38,16 +37,7 @@ public class Driver {
         for (Pet pet: pets) {
             System.out.println(pet.returnDetailsString());
             pet.makeAnimalNoise();
-
         }
-
-        // Creation of a pet record
-
-        PetRecord petRecord = new PetRecord("Timmy", "Reptile", 27, "Sam", 280f, "Black");
-
-        System.out.println(petRecord._animalName());
-
-
 
 
     }
