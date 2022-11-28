fa3a7572aeea9b1d4830c5f107c4df9b8f6a71c8
leyton1509 <taylor92@lancaster.ac.uk>
2022-11-28 14:21:13 +0000

    Added the overridden make noise

--- src/PetTypes/DogTypes/GermanShepard.java
+++ src/PetTypes/DogTypes/GermanShepard.java
@@ -7,4 +7,10 @@ public class GermanShepard extends Dog {
     public GermanShepard(String _animalName, String _animalClass, int _animalAge, String _ownerName, float _animalWeight, String _animalColour, String _breed){
         super  ( _animalName,  _animalClass, _animalAge, _ownerName, _animalWeight, _animalColour, _breed);
     }
+
+    @Override
+    public void makeAnimalNoise(){
+        System.out.println("BIG BARK");
+    }
+
 }
